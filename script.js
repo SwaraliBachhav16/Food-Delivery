e78484c2665434278d8a5702ffc7a15167fa18a7
SwaraliBachhav16 <161601562+SwaraliBachhav16@users.noreply.github.com>
2024-05-17 21:20:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+  
+const searchBox=document.querySelectorAll(".card-body input");
+const searchbtn=document.querySelectorAll(".card-body button");
+
+searchbtn.addEventListener("click",()=>{
+   console.log(searchBox.value);
+})
+
+
+
+
